1d6cbbb7e3cad5383afc141777b92c5427eab1cf
Olek Poplavsky <olek@woodenbits.com>
2012-02-25 19:49:11 -0500

    made project compatible with ruby 1.8.7 (previously it was ruby >=1.9.2 only)

--- lib/capistrano/multiproject/any_server_role.rb
+++ lib/capistrano/multiproject/any_server_role.rb
@@ -2,9 +2,11 @@ Capistrano::Configuration.instance.load do
   namespace :multiproject do
     desc "[internal] Generate arteficial any_server role that enumerates all known servers"
     task :any_server_role do
-      project_roles_sym = project_roles.map(&:to_sym)
-      servers = roles.values.map(&:servers).flatten.sort.uniq.map(&:to_s)
-      top.role :any_server, *servers, :no_release => true
+      project_roles_sym = project_roles.map { |o| o.to_sym }
+      servers = roles.values.map { |o| o.servers }.map { |o| o.to_s }.flatten.sort.uniq
+#      ruby 1.8 hates having hash after expanding array, while 1.9 is totally cool with that
+#      top.role(:any_server, *servers, { :no_release => true })
+      servers.each { |s| top.server s, :any_server, :no_release => true }
     end
   end
 
--- lib/capistrano/multiproject/filter_roles.rb
+++ lib/capistrano/multiproject/filter_roles.rb
@@ -2,8 +2,8 @@ Capistrano::Configuration.instance.load do
   namespace :multiproject do
     desc "[internal] Filter roles to only those that are used by the current project"
     task :filter_roles do
-      project_roles_sym = project_roles.map(&:to_sym)
-      roles.select! { |k,v| project_roles_sym.include?(k) }
+      project_roles_sym = project_roles.map { |o| o.to_sym }
+      roles.delete_if { |k,v| !project_roles_sym.include?(k) }
       logger.info "Filtered roles down to '#{roles.keys.sort.join(', ')}'"
       if roles.empty? && project_roles == [project]
         abort "Define servers of role '#{project}' in stage '#{stage}' configuration, or specify project roles in project recipe (e.g. 'set :project_roles, [:foo, :bar]')"
