498593d2ccad1bed2844361050c89447f9c593fc
Olek Poplavsky <olek@woodenbits.com>
2012-03-15 10:55:22 -0400

    flattening after doing to_s on arrays does you no good, fixed this bug

--- lib/capistrano/multiproject/any_server_role.rb
+++ lib/capistrano/multiproject/any_server_role.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance.load do
     desc "[internal] Generate arteficial any_server role that enumerates all known servers"
     task :any_server_role do
       project_roles_sym = project_roles.map { |o| o.to_sym }
-      servers = roles.values.map { |o| o.servers }.map { |o| o.to_s }.flatten.sort.uniq
+      servers = roles.values.map { |o| o.servers }.flatten.map { |o| o.to_s }.sort.uniq
 #      ruby 1.8 hates having hash after expanding array, while 1.9 is totally cool with that
 #      top.role(:any_server, *servers, { :no_release => true })
       servers.each { |s| top.server s, :any_server, :no_release => true }
